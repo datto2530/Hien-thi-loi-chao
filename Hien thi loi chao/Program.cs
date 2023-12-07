59679a006c9c1f812cd89a7cdb3d38d09847f7c4
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-07 16:34:33 +0700

    Da xong bai hien thi loi chao

---
+++ Hien thi loi chao/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+namespace SystemConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap ten cua ban: ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello " +  yourName);
+        }
+    }
+}
+
+
+
